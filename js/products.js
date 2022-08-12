0a98a9734d7defc91267799072e36c5434bf7e53
Romi Vanega <66981385+rosettastoned23@users.noreply.github.com>
2022-08-12 15:31:16 -0300

    Update products.js

--- js/products.js
+++ js/products.js
@@ -4,16 +4,16 @@ fetch(productsAutos)
     .then(response => response.json())
     .then(data => {
         console.log(data)
-        let autos_Prod = document.getElementById("productos")
-    for (i=0 ; i<data.productos.length; i++){
+        let autos_Prod = document.getElementById("products")
+    for (i=0 ; i<data.products.length; i++){
         autos_Prod.innerHTML +=
         `<div class= "container-autos">
-            <div class= "prod-name">${data.productos[i].name}</div>
-            <div class= "prod-cost">${data.productos[i].cost}</div>
-            <div class= "prod-currency">${data.productos[i].currency}</div>
-            <div class= "prod-description">${data.productos[i].description}</div>
-            <div class= "prod-image"><img src= "${data.productos[i].image}"</div>
-            <div class= "prod-soldCount">${data.productos[i].soldCount}vendidos</div>
+            <div class= "prod-name">${data.products[i].name}</div>
+            <div class= "prod-cost">${data.products[i].cost}</div>
+            <div class= "prod-currency">${data.products[i].currency}</div>
+            <div class= "prod-description">${data.products[i].description}</div>
+            <div class= "prod-image"><img src= "${data.products[i].image}"</div>
+            <div class= "prod-soldCount">${data.products[i].soldCount}vendidos</div>
         </div>`
     }
 });
