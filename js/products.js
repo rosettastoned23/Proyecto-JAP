1f2ac2aa731126640516e6a7902c5e72929993aa
Romi Vanega <66981385+rosettastoned23@users.noreply.github.com>
2022-08-12 15:34:33 -0300

    Update products.js

--- js/products.js
+++ js/products.js
@@ -9,11 +9,12 @@ fetch(productsAutos)
         autos_Prod.innerHTML +=
         `<div class= "container-autos">
             <div class= "prod-name">${data.products[i].name}</div>
-            <div class= "prod-cost">${data.products[i].cost}</div>
-            <div class= "prod-currency">${data.products[i].currency}</div>
-            <div class= "prod-description">${data.products[i].description}</div>
-            <div class= "prod-image"><img src= "${data.products[i].image}"</div>
-            <div class= "prod-soldCount">${data.products[i].soldCount}vendidos</div>
+            <div class= "products-name">${data.products[i].name}</div>
+            <div class= "products-cost">${data.products[i].cost}</div>
+            <div class= "products-currency">${data.products[i].currency}</div>
+            <div class= "products-description">${data.products[i].description}</div>
+            <div class= "products-image"><img src= "${data.products[i].image}"</div>
+            <div class= "products-soldCount">${data.products[i].soldCount}vendidos</div>
         </div>`
     }
-});
+})
