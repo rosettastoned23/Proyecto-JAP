a30a7db0634eeb2a5a15459ddf717ece7d52ef76
Romi Vanega <66981385+rosettastoned23@users.noreply.github.com>
2022-08-17 14:38:38 -0300

    entrega-parte-2

--- js/products.js
+++ js/products.js
@@ -1,19 +1,35 @@
+document.addEventListener('DOMContentLoaded', function(){
+
 const productsAutos = "https://japceibal.github.io/emercado-api/cats_products/101.json"
 
 fetch(productsAutos)
     .then(response => response.json())
     .then(data => {
         console.log(data)
-        let autos_Prod = document.getElementById("products")
-    for (i=0 ; i<data.products.length; i++){
-        autos_Prod.innerHTML +=
-        `<div class= "container-autos">
-            <div class= "products-name">${data.products[i].name}</div>
-            <div class= "products-cost">${data.products[i].cost}</div>
-            <div class= "products-currency">${data.products[i].currency}</div>
-            <div class= "products-description">${data.products[i].description}</div>
-            <div class= "products-image"><img src= "${data.products[i].image}"</div>
-            <div class= "products-soldCount">${data.products[i].soldCount}vendidos</div>
+        let autosProd = document.getElementById("products");
+        
+    for (let i=0 ; i < data.products.length; i++){
+        //console.log(data.products[3].name)
+      autosProd.innerHTML +=
+        `<div class="container-autos">
+            <div onclick="setCatID(${data.products[i].id})" class="list-group-item list-group-item-action cursor-active">
+                <div class="row">
+                    <div class="col-3">
+                        <img src= "${data.products[i].image}" alt="${data.products[i].description}" class="img-thumbnail"">
+                    </div>
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <div id="datos-auto">
+                                <h4 class="mb-1">${data.products[i].name} - ${data.products[i].currency} -  ${data.products[i].cost}</h4>
+                            </div>
+                            <small class="text-muted">${data.products[i].soldCount} Vendidos</small>
+                        </div>
+                        <p class="mb-1">${data.products[i].description}</p>
+                    </div>
+                    
+                </div>
+            </div>
         </div>`
     }
+})
 })
